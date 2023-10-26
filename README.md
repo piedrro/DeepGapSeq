ef237bdf5cd4ee3847ed257a78cb2a9ee606860b
turnerp <piers.turner@protonmail.com>
2023-10-26 16:27:58 +0100

    working basic trace generator

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # DeepGapSeq
+
+## Installing DeepGapSeq From GitHub
+
+    conda create –-name DeepGapSeq python==3.9
+    conda activate DeepGapSeq
+    conda install -c anaconda git
+    conda update --all
+
+    git clone https://github.com/piedrro/DeepGapSeq.git
+
+    cd DeepGapSeq
+
+    pip install -r requirements.txt
+
---
+++ generate_traces_example.py
@@ -0,0 +1,14 @@
+
+
+
+
+from simulation.deepgapseq_trace_generator import trace_generator
+
+
+generator = trace_generator(n_colors=1, 
+                            n_states=2,
+                            n_frames=500,
+                            n_traces=100,
+                            export_mode="text_files")
+
+training_data, training_labels = generator.generate_traces()
